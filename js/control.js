0bc99ea26e3bdb3038ba845a23928d9a807aa70b
Mars <doraemonlsvt@gmail.com>
2023-12-23 00:14:28 +0700

    final maybe

---
+++ js/control.js
@@ -0,0 +1,15 @@
+audioStartsAt("ChuyenCanh", 3);
+tangDan("ChuyenCanh", 0.25);
+
+pauseAfterSeconds("Dramatic", 3, 0.2, 0.5);
+
+audioStartsAt("Camera", 2);
+setVolume("Camera", 0.7);
+audioStartsAt("Camera2", 2);
+audioStartsAt("Camera3", 1.5);
+
+audioStartsAt("Romantic", 0);
+tangDan("Romantic", 0.2, 0.5, 0.05, 0.5);
+
+audioStartsAt("Nhac_dc", 93);
+tangDan("Nhac_dc", 0.2, 1);
